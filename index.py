8bd342abf07a55d3c694736a07fc57fd106c4c9d
tianxueren <1770664399@qq.com>
2023-05-31 11:31:10 +0800

    拗音

--- index.py
+++ index.py
@@ -1,59 +1,4 @@
-# 清音/平假名/写
-voiceless_holiday_write = [
-    "あ",
-    "い",
-    "う",
-    "え",
-    "お",
-    "か",
-    "き",
-    "く",
-    "け",
-    "こ",
-    "さ",
-    "し",
-    "す",
-    "せ",
-    "そ",
-    "た",
-    "ち",
-    "つ",
-    "て",
-    "と",
-    "な",
-    "に",
-    "ぬ",
-    "ね",
-    "の",
-    "は",
-    "ひ",
-    "ふ",
-    "へ",
-    "ほ",
-    "ま",
-    "み",
-    "む",
-    "め",
-    "も",
-    "や",
-    "い",
-    "ゆ",
-    "え",
-    "よ",
-    "ら",
-    "り",
-    "る",
-    "れ",
-    "ろ",
-    "わ",
-    "い",
-    "う",
-    "え",
-    "を",
-    "ん",
-]
-
-# 清音/假名/读
+# 清音/读 (gif)
 voiceless_read = [
     "a",
     "i",
@@ -106,6 +51,61 @@ voiceless_read = [
     "e",
     "wo",
     "n",
+]
+
+# 清音/平假名/写 (mp3)
+voiceless_holiday_write = [
+    "あ",
+    "い",
+    "う",
+    "え",
+    "お",
+    "か",
+    "き",
+    "く",
+    "け",
+    "こ",
+    "さ",
+    "し",
+    "す",
+    "せ",
+    "そ",
+    "た",
+    "ち",
+    "つ",
+    "て",
+    "と",
+    "な",
+    "に",
+    "ぬ",
+    "ね",
+    "の",
+    "は",
+    "ひ",
+    "ふ",
+    "へ",
+    "ほ",
+    "ま",
+    "み",
+    "む",
+    "め",
+    "も",
+    "や",
+    "い",
+    "ゆ",
+    "え",
+    "よ",
+    "ら",
+    "り",
+    "る",
+    "れ",
+    "ろ",
+    "わ",
+    "い",
+    "う",
+    "え",
+    "を",
+    "ん",
 ]
 
 # 清音/片假名/写
@@ -163,7 +163,7 @@ voiceless_film_leave_write = [
     "ン",
 ]
 
-# 浊音/平假名/写
+# 浊音/平假名/写 (mp3)
 vibrant_holiday_write = [
     "が",
     "ぎ",
@@ -250,17 +250,98 @@ vibrant_film_leave_write = [
     "ポ",
 ]
 
+# 拗音/平假名 (mp3)
+audible_ping_write = [
+    "きゃ",
+    "きゅ",
+    "きょ",
+    "ぎゃ",
+    "ぎゅ",
+    "ぎょ",
+    "しゃ",
+    "しゅ",
+    "しょ",
+    "じゃ",
+    "じゅ",
+    "じょ",
+    "ちゃ",
+    "ちゅ",
+    "ちょ",
+    "ぢゃ",
+    "ぢゅ",
+    "ぢょ",
+    "にゃ",
+    "にゅ",
+    "にょ",
+    "ひゃ",
+    "ひゅ",
+    "ひょ",
+    "びゃ",
+    "びゅ",
+    "びょ",
+    "ぴゃ",
+    "ぴゅ",
+    "ぴょ",
+    "みゃ",
+    "みゅ",
+    "みょ",
+    "りゃ",
+    "りゅ",
+    "りょ",
+]
+
+# 拗音/片假名
+audible_pian_write = [
+    "キャ",
+    "キュ",
+    "キョ",
+    "ギャ",
+    "ギュ",
+    "ギョ",
+    "シャ",
+    "シュ",
+    "ショ",
+    "ジャ",
+    "ジュ",
+    "ジョ",
+    "チャ",
+    "チュ",
+    "チョ",
+    "ヂャ",
+    "ヂュ",
+    "ヂョ",
+    "ニャ",
+    "ニュ",
+    "ニョ",
+    "ヒャ",
+    "ヒュ",
+    "ヒョ",
+    "ビャ",
+    "ビュ",
+    "ビョ",
+    "ピャ",
+    "ピュ",
+    "ピョ",
+    "ミャ",
+    "ミュ",
+    "ミョ",
+    "リャ",
+    "リュ",
+    "リョ",
+]
+
 
 host = " https://res.hjfile.cn/pt/m/jp/50yin"
-import requests, os
+import requests, os, time
 
 
 # 下载mp3
-def down_mp3(str):
+def down_mp3(path_str, str):
     url = f"{host}/audio/{str}.mp3"
     req = requests.get(url)
+    time.sleep(0.8)
     os.makedirs(f"./mp3", exist_ok=True)
-    with open(f"./mp3/{str}.mp3", "wb") as f:
+    with open(f"./mp3/{path_str}{str}.mp3", "wb") as f:
         f.write(req.content)
 
 
@@ -268,24 +349,36 @@ def down_mp3(str):
 def down_gif(type, str, index, name):
     url = f"{host}/img/gif/{type}/{type}_gif_{str}{index}.gif"
     req = requests.get(url)
+    time.sleep(0.8)
     os.makedirs(f"./gif/{type}", exist_ok=True)
     with open(f"./gif/{type}/{name}.gif", "wb") as f:
         f.write(req.content)
 
 
 def get_down():
-    for text in voiceless_holiday_write + vibrant_film_leave_write:
-        print(f"downloading {text} mp3 source")
-        down_mp3(text)
-
+    # 下载所有 gif
     for index, text in enumerate(voiceless_read):
+        # 平/片假名
         for type in ["ping", "pian"]:
             print(f"downloading {type} / {text} source")
             down_gif(type, text, index + 1, voiceless_holiday_write[index])
 
+    # 下载所有mp3
+    for _path, data in [
+        ("", voiceless_holiday_write),
+        ("vibrant/", vibrant_holiday_write),
+        ("audible/", audible_ping_write),
+    ]:
+        for text in data:
+            print(f"downloading mp3 / {text} source")
+            down_mp3(_path, text)
 
-# get_down()
+
+get_down()
 
 
-
-import 
+# for i, oo in enumerate(vibrant_read):
+#     if i % 5 == 0:
+#         print("-----------")
+#     print(vibrant_read[i])
+#     print(f"{vibrant_holiday_write[i]} {vibrant_film_leave_write[i]} ")
